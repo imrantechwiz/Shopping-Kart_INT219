24d9078329429be0283e1ee54c646ad8c666a047
Imran Khan <imran.techwiz@gmail.com>
2021-11-12 21:38:16 +0530

    products updated

--- products.js
+++ products.js
@@ -3,25 +3,25 @@ var products = {
   123: {
     name : "Macbook Pro",
     desc : "Boosted with M1 chip ",
-    img : "dummy-pdt-b.jpg",
+    img : "pro.jpg",
     price : 129000
   },
   124: {
     name : "Microsoft Surface Pro",
     desc : "Boosted with Windows 11",
-    img : "dummy-pdt-b.jpg",
+    img : "surface.jfif",
     price : 79999
   },
   125: {
     name : "iPhone 12 Max",
     desc : "Boosted with M1 chip ",
-    img : "dummy-pdt-a.png",
+    img : "ip.jfif",
     price : 65000
   },
   127: {
     name : "Apple SmartWatch",
     desc : "Boosted with M1 chip ",
-    img : "dummy-pdt-a.png",
+    img : "iw.jfif",
     price : 24999
   },
 };
