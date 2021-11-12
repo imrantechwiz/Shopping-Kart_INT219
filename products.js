0d209367c0b4661ff765e8e87d13aed8879d7c36
Imran Khan <56226186+imrantechwiz@users.noreply.github.com>
2021-11-12 12:54:42 +0530

    Cart created -- JS functions added

---
+++ products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Macbook Pro",
+    desc : "Boosted with M1 chip ",
+    img : "dummy-pdt-b.jpg",
+    price : 129000
+  },
+  124: {
+    name : "Microsoft Surface Pro",
+    desc : "Boosted with Windows 11",
+    img : "dummy-pdt-b.jpg",
+    price : 79999
+  },
+  125: {
+    name : "iPhone 12 Max",
+    desc : "Boosted with M1 chip ",
+    img : "dummy-pdt-a.png",
+    price : 65000
+  },
+  127: {
+    name : "Apple SmartWatch",
+    desc : "Boosted with M1 chip ",
+    img : "dummy-pdt-a.png",
+    price : 24999
+  },
+};
